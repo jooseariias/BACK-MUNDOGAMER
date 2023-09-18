9c8effd93d3fafffefeb864a187ab149fd24eeda
jooseariias <arijoose@gmail.com>
2023-09-18 08:03:09 -0300

    v1

--- index.js
+++ index.js
@@ -20,6 +20,7 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const { initiateTables } = require("./src/seeders/dbSeeder");
+const PORT = process.env.PORT || 3001
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
   initiateTables();
